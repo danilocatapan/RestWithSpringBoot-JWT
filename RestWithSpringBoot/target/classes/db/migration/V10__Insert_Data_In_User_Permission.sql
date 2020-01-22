b78467a1d40ab9506b96cb6eccdb24dc8ca1c87e
Danilo <dcatapan@gmail.com>
2020-01-22 20:42:17 -0300

    targed deleted

--- RestWithSpringBoot/target/classes/db/migration/V10__Insert_Data_In_User_Permission.sql
+++
@@ -1,6 +0,0 @@
-﻿INSERT INTO `user_permission` (`id_user`, `id_permission`) VALUES
-	(1, 1),
-	(1, 2),
-	(2, 1),
-	(3, 1),
-	(3, 2);
--- RestWithSpringBoot/target/classes/db/migration/V8__Insert_Data_In_Users.sql
+++
@@ -1,4 +0,0 @@
-﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
-    ('danilo', 'Danilo Catapan', '$2a$16$29Hyw/6iiACPN4o8drhWeOyRbamjxGhYi.D8PyBn14JiCHkeFrd8q', b'1', b'1', b'1', b'1'),
-	('leandro', 'Leandro Costa', '$2a$16$9qr2tv0HmXbHBsx.TZFjfux742wCZM32a8Wi6iBqwIqaizlHPuxHS', b'1', b'1', b'1', b'1'),
-	('flavio', 'Flavio Costa', '$2a$16$h4yDQCYTy62R6xrtFDWONeMH3Lim4WQuU/aj8hxW.dJJoeyvtEkhK', b'1', b'1', b'1', b'1');
